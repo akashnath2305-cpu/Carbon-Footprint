17f337f8d8c32309e82641826953fff2108a3f0b
akashnath2305-cpu <akashnath2305@gmail.com>
2026-06-16 12:25:47 +0530

    Updates and Minor Fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
       include: ['canvas-confetti']
     },
     build: {
+      sourcemap: true,
       rollupOptions: {
         output: {
           manualChunks(id) {
